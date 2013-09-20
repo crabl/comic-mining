08785a1b9ca6175e942aa887ac31fe116b5a3045
Chris Rabl <rabl.christopher@gmail.com>
2013-09-20 03:24:59 -0600

    AJAX search now works, still need to work out back button rules

--- public/javascripts/controllers/CharacterSearchController.js
+++ public/javascripts/controllers/CharacterSearchController.js
@@ -1,17 +1,13 @@
 var CharacterSearchController = function() {
-
-    this.characterSearch = function() {
-	$.ajax({
-	    url: "/logout",
-	    type: "POST",
-	    data: {logout : true},
-	    success: function(data) {
-		this.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
-	    },
-	    error: function(jqXHR) {
-		console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
-	    }
-	});
+    this.showSearchResults = function(data) {
+	for(i = 0; i < data.number_of_page_results; i++) {
+	    $('div#results').append('<div>'
+	    +'<h2>'+data.results[i].name+'</h2>'
+	    +'<img class="search-icon-image" src="'+data.results[i].image.icon_url+'" />'
+	    +'<p>Real Name: '+data.results[i].real_name+'</p>'
+            +'<p>Appears in <strong>'+data.results[i].count_of_issue_appearances+'</strong> issues</p>'
+            +'<p>First appeared in issue <a href="/issue/'+data.results[i].first_appeared_in_issue.id+'">"'+data.results[i].first_appeared_in_issue.name+'"</a></p>'
+	    +'</div>');
+	}
     };
-
 };
